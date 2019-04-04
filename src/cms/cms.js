d8c5e7c6afe831270a542606b6767c2585f76ae9
Matt Moriarity <matt@mattmoriarity.com>
2019-04-03 22:21:08 -0600

    Add preview widget for microblog posts
    I don't think it's working though.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from "netlify-cms";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
+import MicroblogPostPreview from "./preview-templates/MicroblogPostPreview";
 
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("micro", MicroblogPostPreview);
---
+++ src/cms/preview-templates/MicroblogPostPreview.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { MicroblogPostTemplate } from "../../templates/microblog-post";
+
+const MicroblogPostPreview = ({ widgetFor }) => (
+  <MicroblogPostTemplate content={widgetFor("body")} />
+);
+
+MicroblogPostPreview.propTypes = {
+  widgetFor: PropTypes.func
+};
+
+export default MicroblogPostPreview;
