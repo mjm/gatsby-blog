ddd4d8cd7b777c8162b92f15bf165ae3f4508b0a
Matt Moriarity <matt@mattmoriarity.com>
2019-04-04 14:45:16 -0600

    Update template for longform blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,91 +1,85 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
 
 export const BlogPostTemplate = ({
   content,
   contentComponent,
+  date,
   description,
-  tags,
-  title,
   helmet,
+  isoDate,
+  title,
 }) => {
-  const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content;
 
   return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
+    <article className="section">
+      {helmet || ""}
+      <h1 className="p-name">{title}</h1>
+      <div className="e-content">
+        <p>{description}</p>
+        <PostContent content={content} />
       </div>
-    </section>
-  )
-}
+      <div className="text-right mt-4">
+        <time
+          className="dt-published text-xs py-2 px-3 text-purple-600 bg-purple-100 rounded-lg uppercase no-underline"
+          dateTime={isoDate}
+        >
+          {date}
+        </time>
+      </div>
+    </article>
+  );
+};
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
+  date: PropTypes.string,
   description: PropTypes.string,
-  title: PropTypes.string,
   helmet: PropTypes.object,
-}
+  isoDate: PropTypes.string,
+  title: PropTypes.string,
+};
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
       <BlogPostTemplate
         content={post.html}
         contentComponent={HTMLContent}
+        date={post.frontmatter.date}
         description={post.frontmatter.description}
         helmet={
           <Helmet titleTemplate="%s | Blog">
-            <title>{`${post.frontmatter.title}`}</title>
+            <title>{post.frontmatter.title}</title>
             <meta
               name="description"
               content={`${post.frontmatter.description}`}
             />
           </Helmet>
         }
+        isoDate={post.frontmatter.isoDate}
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
       />
     </Layout>
-  )
-}
+  );
+};
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
+    markdownRemark: PropTypes.object
+  })
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
@@ -93,11 +87,12 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "MMM D, Y")
+        isoDate: date(formatString: "YYYY-MM-DDTHH:mm:ssZ")
         title
         description
         tags
       }
     }
   }
-`
+`;
