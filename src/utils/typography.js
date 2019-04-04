df3a4122e3d7fbcfd2861adac7aeb833ed6e9e63
Matt Moriarity <matt@mattmoriarity.com>
2019-04-03 18:05:30 -0600

    Set up typography

---
+++ src/utils/typography.js
@@ -0,0 +1,15 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: "20px",
+  baseLineHeight: 1.5,
+  googleFonts: [
+    { name: "Eczar", styles: ["400", "500"] },
+    { name: "Gentium Basic", styles: ["400", "400i", "700", "700i"] }
+  ],
+  headerFontFamily: ["Eczar"],
+  headerWeight: 500,
+  bodyFontFamily: ["Gentium Basic"]
+});
+
+export default typography;
