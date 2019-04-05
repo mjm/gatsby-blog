7e19fe9f9330c766b1cea52157ba7c4a27cc4513
Matt Moriarity <matt@mattmoriarity.com>
2019-04-05 13:29:28 -0600

    Add IndieAuth links to head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,29 +15,10 @@ const TemplateWrapper = ({ children }) => {
         <title>{title}</title>
         <meta name="description" content={description} />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-16x16.png"
-          sizes="16x16"
-        />
+        <link rel="me" href="https://github.com/mjm" />
+        <link rel="me" href="mailto:matt@mattmoriarity.com" />
+        <link rel="authorization_endpoint" href="https://indieauth.com/auth" />
 
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
