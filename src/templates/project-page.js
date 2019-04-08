5cf82651a3cb1836c5f8d9561674c04c0839a61d
Matt Moriarity <matt@mattmoriarity.com>
2019-04-07 21:33:17 -0600

    Add badge for repo on project page

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -3,13 +3,31 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-export const ProjectPageTemplate = ({ title, content, contentComponent }) => {
+export const ProjectPageTemplate = ({
+  title,
+  repository,
+  content,
+  contentComponent
+}) => {
   const PageContent = contentComponent || Content;
 
   return (
     <article className="h-entry mt-12 mb-10">
       <h2 className="p-name">{title}</h2>
+      {repository && (
+        <div className="-mt-1 mb-3 text-sm">
+          <a
+            href={`https://github.com/${repository}`}
+            className="no-underline inline-flex items-center py-1 px-2 bg-purple-100 rounded border-solid border border-purple-200"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+            <span className="ml-2 text-xs">{repository}</span>
+          </a>
+        </div>
+      )}
       <PageContent className="e-content" content={content} />
     </article>
   );
@@ -29,6 +47,7 @@ const ProjectPage = ({ data }) => {
       <ProjectPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
+        repository={post.frontmatter.repository}
         content={post.html}
       />
     </Layout>
@@ -47,6 +66,7 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        repository
       }
     }
   }
