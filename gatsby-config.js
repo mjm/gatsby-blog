fd0b5ce4669040c41d89c282ac7f2701af4aca67
Matt Moriarity <matt@mattmoriarity.com>
2019-04-08 17:15:41 -0600

    Fix purgecss config
    I use very little of Tailwind's classes, so this makes a huge difference
    in the size of the CSS.

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,8 @@ module.exports = {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"] // applies purging only on the bulma css file
+        tailwind: true,
+        purgeOnly: ['/all.css']
       }
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify" // make sure to keep it last in the array
