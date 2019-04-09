3866bc87bd24e0ecd7c44a53a4cb4496a76aa993
Matt Moriarity <matt@mattmoriarity.com>
2019-04-08 22:30:51 -0600

    Remove unused coffee-related images

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images",
-      },
-    },
-    {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
