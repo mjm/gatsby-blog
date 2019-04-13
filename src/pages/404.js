dddc9de5a69caf75d281cd733e0a3106dd24bb06
Matt Moriarity <matt@mattmoriarity.com>
2019-04-12 22:18:25 -0600

    Add a better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,15 @@ import Layout from "../components/Layout"
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <article className="mt-12 mb-10">
+      <h2>Couldn't find it!</h2>
+      <p>Sorry, but the page you're looking for doesn't seem to be here.</p>
+      <p>
+        I've tried really hard not to break any links here since the beginning
+        of 2018, but I may have missed something. Or the URL may be mistyped.
+        Who knows?
+      </p>
+    </article>
   </Layout>
 )
 
