adc50fcdb1802705786154845c4fa00aea0de795
Matt Moriarity <matt@mattmoriarity.com>
2019-04-14 09:21:52 -0600

    Remove Netlify CMS
    I find Forestry.io works better for this site's needs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,12 +43,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
     "gatsby-plugin-force-trailing-slashes",
     "gatsby-plugin-catch-links",
     {
--- src/cms/cms.js
+++
@@ -1,9 +0,0 @@
-import CMS from "netlify-cms"
-
-import BlogPostPreview from "./preview-templates/BlogPostPreview"
-import MicroblogPostPreview from "./preview-templates/MicroblogPostPreview"
-import StaticPagePreview from "./preview-templates/StaticPagePreview"
-
-CMS.registerPreviewTemplate("about", StaticPagePreview)
-CMS.registerPreviewTemplate("blog", BlogPostPreview)
-CMS.registerPreviewTemplate("micro", MicroblogPostPreview)
