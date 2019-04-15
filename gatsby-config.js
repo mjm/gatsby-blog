3617609390e53af8b0c7306fdbe74f020b69f032
Matt Moriarity <matt@mattmoriarity.com>
2019-04-15 16:55:02 -0600

    Remove Tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-var proxy = require("http-proxy-middleware")
-
 module.exports = {
   siteMetadata: {
     title: "Matt Moriarity",
@@ -101,20 +99,12 @@ module.exports = {
         implementation: require("sass"),
       },
     },
-    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-plugin-typography",
       options: {
         pathToConfigModule: "src/utils/typography",
       },
     },
-    {
-      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
-      options: {
-        tailwind: true,
-        purgeOnly: ["/all.css"],
-      },
-    }, // must be after other CSS plugins
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 }
