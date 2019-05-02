df927e1f4dbea4d347a063469a4035a48b9cfa9f
Matt Moriarity <matt@mattmoriarity.com>
2019-05-02 11:46:31 -0600

    Add plain text error handler

--- api/micropub/app.ts
+++ api/micropub/app.ts
@@ -10,3 +10,34 @@ if (process.env.NODE_ENV !== "test") {
 
 export const router = promiseRouter()
 app.use(router)
+
+app.use(
+  (
+    err: any,
+    _req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    if (!err) {
+      next()
+    }
+
+    if (res.headersSent) {
+      next(err)
+    }
+
+    const env = process.env.NODE_ENV || "development"
+    const prodMessage = err.message || err.toString()
+    const devMessage = err.stack || prodMessage
+    const status = err.statusCode || 500
+
+    if (env !== "test") {
+      console.error(devMessage)
+    }
+
+    res
+      .type("text")
+      .status(status)
+      .send(env === "development" ? devMessage : prodMessage)
+  }
+)
