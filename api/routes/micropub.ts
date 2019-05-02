d2b8cf585b34929ef199e3af21d0bfdf2282f03e
Matt Moriarity <matt@mattmoriarity.com>
2019-05-02 10:29:13 -0600

    Combine get and post, test wildcard

--- api/routes/micropub.ts
+++ api/routes/micropub.ts
@@ -1,20 +1,36 @@
+import beeline from "../micropub/honeycomb"
 import express from "express"
 import multer from "multer"
 import httpError from "http-errors"
-import { app, router } from "../../micropub/app"
-import { requireToken } from "../../micropub/auth"
-import { baseUrl } from "../../micropub/config"
-import * as postMiddleware from "../../micropub/middleware"
+import { app, router } from "../micropub/app"
+import { requireToken } from "../micropub/auth"
+import { baseUrl } from "../micropub/config"
+import * as postMiddleware from "../micropub/middleware"
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
-router.use(express.json())
-router.use(express.urlencoded({ extended: true }))
+router.get(
+  "*",
+  requireToken,
+  async (req: express.Request, res: express.Response) => {
+    beeline.customContext.add("micropub.q", req.query.q)
+
+    if (req.query.q === "config") {
+      res.send({
+        "media-endpoint": "https://gatsby-blog.mjm.now.sh/api/micropub/media",
+      })
+    } else {
+      throw new httpError.BadRequest("unknown 'q' value")
+    }
+  }
+)
 
 router.post(
   "*",
   requireToken,
+  express.json(),
+  express.urlencoded({ extended: true }),
   upload.fields([
     { name: "photo", maxCount: 8 },
     { name: "photo[]", maxCount: 8 },
